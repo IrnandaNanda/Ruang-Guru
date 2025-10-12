9bfefe25c60a6e428744cdd7db21c8ff105e093d
IrnandaNanda <irnandatri1509@gmail.com>
2025-10-12 13:54:39 +0700

    done exercise profit monitor

--- Frontend/Pemrogaman Frontend Dasar with Javascript/js-exercise-conditional-profit-monitor-v1/main.js
+++ Frontend/Pemrogaman Frontend Dasar with Javascript/js-exercise-conditional-profit-monitor-v1/main.js
@@ -1,9 +1,17 @@
 function profitMonitor(todayProfit, lastProfit) {
-  return null; // TODO: replace this
+  if (todayProfit < lastProfit) {
+    console.log(`Profit turun sebanyak ${lastProfit - todayProfit} point`)
+  } else if (todayProfit > lastProfit) {
+    console.log(`Profit naik sebanyak ${todayProfit - lastProfit} point`)
+  } else {
+    console.log("Profit stabil")
+  }
 }
 
 console.log(profitMonitor(100, 50));
 console.log(profitMonitor(50, 100));
 console.log(profitMonitor(100, 100));
+console.log(profitMonitor(200, 100))
+console.log(profitMonitor(100,150))
 
 module.exports = profitMonitor;
