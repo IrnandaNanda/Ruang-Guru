223c9763739268edc812fdedef22adc9c2ca7b3d
IrnandaNanda <irnandatri1509@gmail.com>
2025-10-13 12:49:18 +0700

    done conditional rock-paper-scissor

--- Frontend/Pemrogaman Frontend Dasar with Javascript/js-exercise-advance-conditional-rock-paper-scissor-v3/main.js
+++ Frontend/Pemrogaman Frontend Dasar with Javascript/js-exercise-advance-conditional-rock-paper-scissor-v3/main.js
@@ -1,11 +1,31 @@
 function rockPaperScissor(player1, player2) {
-    return null // TODO: replace this
+
+  if (player1 == "paper" && player2 == "rock") {
+    return console.log("player1 Won!")
+  } else if (player1 == "rock" && player2 == "paper") {
+    return console.log("player2 Won!")
   }
-  
-  console.log(rockPaperScissor('scissor', 'paper')); // "Player 1 Won!"
-  console.log(rockPaperScissor('rock', 'paper')); // "Player 2 Won!"
-  console.log(rockPaperScissor('paper', 'paper')); // "Draw!"
-  console.log(rockPaperScissor('rock', 'rock')); // "Draw!"
-  
-  module.exports = rockPaperScissor;
-  
+
+  if (player1 == "rock" && player2 == "scissor") {
+    return console.log("player1 Won!")
+  } else if (player1 == "scissor" && player2 == "rock") {
+    return console.log("player2 Won!")
+  }
+
+  if (player1 == "scissor" && player2 == "paper") {
+    return console.log("player1 Won!")
+  } else if (player1 == "paper" && player2 == "scissor") {
+    return console.log("player2 Won!")
+  }
+
+  if (player1 === player2) {
+    return console.log("draw")
+  }
+}
+
+console.log(rockPaperScissor("scissor", "paper")); // "Player 1 Won!"
+console.log(rockPaperScissor("rock", "paper")); // "Player 2 Won!"
+console.log(rockPaperScissor("paper", "paper")); // "Draw!"
+console.log(rockPaperScissor("rock", "rock")); // "Draw!"
+
+module.exports = rockPaperScissor;
