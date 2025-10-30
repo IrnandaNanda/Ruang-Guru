fb79f3ddd7f4edc3d7a30d6e30b9eac518cd7f64
IrnandaNanda <irnandatri1509@gmail.com>
2025-10-30 09:20:17 +0700

    Props Children

--- Frontend/Dasar Pemrogaman Frontend dengan React/my-app/src/App.js
+++ Frontend/Dasar Pemrogaman Frontend dengan React/my-app/src/App.js
@@ -1,5 +1,5 @@
 import Title from "./components/Title";
-import Button from "./components/Button"
+import {Button, CustomButton}  from "./components/Button"
 import { Footer, Paragraph } from "./components/Paragraph";
 
 function App() {
@@ -11,6 +11,9 @@ function App() {
         <Button name='Button menggunakan component & Props'/>
         <br/>
         <Button/>
+        <div>
+          <CustomButton textButton='Edit'/>
+        </div>
         <Footer/>
       </div>
     </>
--- Frontend/Dasar Pemrogaman Frontend dengan React/my-app/src/components/Button.js
+++ Frontend/Dasar Pemrogaman Frontend dengan React/my-app/src/components/Button.js
@@ -1,15 +1,19 @@
-function Button(Props) {
+function CustomButton(Props) {
+  return <button>{Props.textButton}</button>;
+}
 
-  // Destruciring 
-  const {name = 'Button Menggunakan Default Props'} = Props
+function Button(Props) {
+  // Destruciring
+  const { name = "Button Menggunakan Default Props" } = Props;
 
   return (
     <>
       <button id="button1" onClick={() => alert("Button Clicked")}>
         {name}
+        <div>{Props.children}</div>
       </button>
     </>
   );
 }
 
-export default Button
+export { Button, CustomButton };
