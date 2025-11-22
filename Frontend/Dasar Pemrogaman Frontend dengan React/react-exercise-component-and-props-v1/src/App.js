e6b8f4f075e5cbc78f4417b72edf4e7aadb689a5
IrnandaNanda <irnandatri1509@gmail.com>
2025-11-22 13:55:23 +0700

    Done Exercise 2

--- Frontend/Dasar Pemrogaman Frontend dengan React/react-exercise-component-and-props-v1/src/App.js
+++ Frontend/Dasar Pemrogaman Frontend dengan React/react-exercise-component-and-props-v1/src/App.js
@@ -1,13 +1,24 @@
 const App = () => {
   return (
-    <div>
-      <Student name={"Djarot Purnomo"} isStudent={true} />
-    </div>
+    <>
+      <div>
+        <Student name={"Djarot Purnomo"} isStudent={true} />
+      </div>
+      <div>
+        <Student name={"Agusman Laia"} isStudent={false} />
+      </div>
+    </>
   );
 };
 
 function Student({ name, isStudent }) {
-  // TODO: answer here
+  return (
+    <>
+      <p>Name: {name}</p>
+      <p>First name: {name.split(" ")[0]}</p>
+      <p>Status: {isStudent ? "Student" : "Not student"}</p>
+    </>
+  );
 }
 
 export default App;
